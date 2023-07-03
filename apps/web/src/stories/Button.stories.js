2698caebc4920c6c08bd8f4d11419c17f2f3f9b3
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-07-03 15:38:38 +0200

    web: paste stories folder from the apps/storybook since that's already converted to tsx + panda

--- apps/web/src/stories/Button.stories.js
+++ apps/web/src/stories/Button.stories.js
@@ -1,44 +1,39 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import { Button } from './Button';
+import { Button } from './Button'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-const meta = {
+export default {
   title: 'Example/Button',
   component: Button,
   tags: ['autodocs'],
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} satisfies Meta<typeof Button>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
+}
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const Primary = {
   args: {
-    primary: true,
+    variant: 'primary',
     label: 'Button',
   },
-};
+}
 
-export const Secondary: Story = {
+export const Secondary = {
   args: {
     label: 'Button',
   },
-};
+}
 
-export const Large: Story = {
+export const Large = {
   args: {
     size: 'large',
     label: 'Button',
   },
-};
+}
 
-export const Small: Story = {
+export const Small = {
   args: {
     size: 'small',
     label: 'Button',
   },
-};
+}
