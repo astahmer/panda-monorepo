1d127c898a2f175b83f46fa52732fb05902218de
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-07-03 14:55:10 +0200

    init storybook with `pnpm dlx storybook@latest init`

---
+++ apps/storybook/.storybook/main.js
@@ -0,0 +1,13 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
